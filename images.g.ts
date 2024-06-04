235ddb01e36c37189bd67028ab643bb44dbfbb60
bruh5743 <morganbyrd97@gmail.com>
2024-06-04 10:52:16 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,64 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Mr Quack":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . f f 5 5 5 5 5 f . . . 
+. . . . f f 5 d 1 f 5 5 f f . . 
+. . . . f 5 5 1 f f 5 5 4 f . . 
+. . . . f 5 5 d f b 5 5 4 4 f . 
+. . f f f f 5 5 5 5 4 4 4 4 4 f 
+f f f 5 5 5 f 5 5 4 4 4 4 4 f . 
+f 5 5 f 5 5 5 f 5 5 5 5 5 f . . 
+f 5 5 5 f 5 5 f 5 5 5 5 5 5 f . 
+f f 5 5 f f f 5 5 5 5 5 5 5 f . 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. . f 5 5 5 5 5 5 5 5 5 f f . . 
+. . . f f f f f f f f f f . . . 
+`;
+            case "image2":
+            case "Baby duck":return img`
+. . . b 5 b . . . . . . . . . . 
+. . . . b 5 b . . . . . . . . . 
+. . . . b b b b b b . . . . . . 
+. . . b 5 5 5 5 5 b b . . . . . 
+. . f d 5 5 1 1 d 5 b b . . . . 
+. . c 4 d 5 f f 1 5 5 b . . . . 
+. . 4 4 d d b f d 5 5 b . . . . 
+b 4 4 4 4 4 5 5 5 5 5 d b b b . 
+. b 4 4 4 4 4 5 5 d b b d d d b 
+. . b 5 5 5 5 5 5 b 5 5 5 d b b 
+. b 5 5 5 5 5 5 d 5 5 5 5 c d c 
+. b 5 5 5 5 5 5 b 5 5 d c d b c 
+. b d 5 5 5 5 5 d b c c d d c . 
+. . b b 5 5 5 d d d d d b c . . 
+. . . b b c c c c c c c c . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "mySong":return hex`0078000408070101001c000f05001202c102c201000405002800000064002800030500060000040c000400080001190c001400011b`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
